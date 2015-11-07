202b3063e97b58ce148c20fc8604ff1e104ce202
vreaxe <vreaxe@gmail.com>
2015-11-07 19:18:33 +0000

    Add config.php

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+
+define( "MAIN_CONTROLLER", "Welcome" );
+define( "MAIN_METHOD_EACH_CTRL", "index" );
+
+define( "ENABLE_LOGS", "0" );
+
+// DATABASE INFO
+define( "DB_HOST", "localhost" );
+define( "DB_USER", "" );
+define( "DB_PASSWORD", "" );
+define( "DB_NAME", "" );
+
+// DEFAULT TEMPLATES
+define( "TEMPLATE_ERROR_404", "errors/404" );
+define( "TEMPLATE_COMMON_ERROR", "errors/error" );
